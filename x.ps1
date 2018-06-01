4ad46f43b95210ec52ae4e6724373b21b0377a2f
glennswest <glennswest@neuralcloudcomputing.com>
2018-06-01 14:07:23 +0000

    Cleanup and add some notes

--- x.ps1
+++
@@ -1,9 +0,0 @@
-$a = Get-NetAdapter | where Name -Match HNSTransparent
-Rename-NetAdapter $a[0].Name -NewName HNSTransparent
-Stop-Service ovs-vswitchd -force; Disable-VMSwitchExtension "Cloudbase Open vSwitch Extension";
-ovs-vsctl --no-wait del-br br-ex
-ovs-vsctl --no-wait --may-exist add-br br-ex
-ovs-vsctl --no-wait add-port br-ex HNSTransparent -- set interface HNSTransparent type=internal
-ovs-vsctl --no-wait add-port br-ex $INTERFACE_ALIAS
-Enable-VMSwitchExtension "Cloudbase Open vSwitch Extension"; sleep 2; Restart-Service ovs-vswitchd
-
