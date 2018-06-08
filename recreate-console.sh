f0a9ca99f120c75ea4be99f982f861e8e77312ae
glennswest <glennswest@neuralcloudcomputing.com>
2018-06-08 02:42:39 +0000

    Appears to be a clean create - route still wip

--- recreate-console.sh
+++ recreate-console.sh
@@ -2,9 +2,6 @@ oc login -u system:admin
 oc delete project openshift-web-console
 sleep 200
 oc adm new-project openshift-web-console --node-selector=''
-#oc delete dc/webconsole
-#oc delete deploy/webconsole
-#oc delete svc/webconsole
 oc process -f console-template.yaml -p "API_SERVER_CONFIG=$(cat console-config.yaml)" | oc apply -n openshift-web-console -f -
-
-
+sleep 10
+oc expose service/webconsole --hostname=gswmon.southcentralus.cloudapp.azure.com --path=/console --port=8443
