d27255900900b783835f403b80bc0306229446dc
glennswest <glennswest@neuralcloudcomputing.com>
2018-06-12 00:23:47 +0000

    minor fixup for recreate consoel
    	modified:   console-template.yaml
    	modified:   recreate-console.sh

--- recreate-console.sh
+++ recreate-console.sh
@@ -4,5 +4,6 @@ sleep 200
 oc adm new-project openshift-web-console --node-selector=''
 oc process -f console-template.yaml -p "API_SERVER_CONFIG=$(cat console-config.yaml)" | oc apply -n openshift-web-console -f -
 sleep 10
-oc expose service/webconsole --hostname=gswmon.southcentralus.cloudapp.azure.com --path=/console --port=443
-oc create route edge  --service service/webconsole --hostname=gswmon.southcentralus.cloudapp.azure.com --path=/console --port=443  --cert=/etc/origin/master/ca.crt --key=/etc/origin/master/ca.key --ca-cert=/etc/origin/master/ca.crt 
+#oc expose service/webconsole --hostname=gswmon.southcentralus.cloudapp.azure.com --path=/console --port=443
+sleep 10
+#oc create route edge  --service service/webconsole --hostname=gswmon.southcentralus.cloudapp.azure.com --path=/console --port=8443  --cert=/etc/origin/master/ca.crt --key=/etc/origin/master/ca.key --ca-cert=/etc/origin/master/ca.crt 
