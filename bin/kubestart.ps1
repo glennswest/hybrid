a41c1f42d0e2d9fba3a9a755ac16c83079b88cff
Glenn West <glennswest@neuralcloudcomputing.com>
2018-06-19 12:43:53 +0000

    Add tls options to kubestart

--- bin/kubestart.ps1
+++ bin/kubestart.ps1
@@ -5,6 +5,7 @@ c:\bin\kubelet.exe --hostname-override=$(hostname) --v=6 `
     --kubeconfig=c:\k\config --hairpin-mode=promiscuous-bridge `
     --image-pull-progress-deadline=20m --cgroups-per-qos=false `
     --enforce-node-allocatable="" --pod-infra-container-image=glennswest/pause:latest `
-    --network-plugin=cni --cni-bin-dir="c:\k\hybrid\bin" --cni-conf-dir "C:\k\hybrid\cni\config"
+    --network-plugin=cni --cni-bin-dir="c:\k\hybrid\bin" --cni-conf-dir "C:\k\hybrid\cni\config" `
+    --tls-cert-file="C:\k\server.crt" --tls-private-key-file="C:\k\server.key"
 
 
