15aeb8f2f841e215e821486537850a94f4e99956
Glenn West <glennswest@neuralcloudcomputing.com>
2018-06-19 14:38:12 +0000

    Setup kubelet service and make sure logs rotate

--- bin/setup_kube_service.ps1
+++ bin/setup_kube_service.ps1
@@ -1,7 +1,14 @@
-nssm set kubelet Application C:\k\hybrid\bin\start_kubeovn.ps1
-nssm set kubelet AppDirectory C:\k\hybrid\bin
-nssm set kubelet AppParameters server
+$powershell = (Get-Command powershell).Source
+$scriptPath = 'C:\k\hybrid\bin\start_kubeovn.ps1'
+$arguments = '-ExecutionPolicy Bypass -NoProfile -File "{0}"' -f $scriptPath
+$serviceName = "kubelet"
+nssm install $serviceName $powershell $arguments
 nssm set kubelet DisplayName Kubelet
 nssm set kubelet Description Kubelet OVN For OpenShift
 nssm set kubelet Start SERVICE_AUTO_START
-
+nssm set kubelet AppStdout C:\k\hybrid\logs\kubelet.log
+nssm set kubelet AppStderr C:\k\hybrid\logs\kubelet.log
+nssm set kubelet AppRotateFiles 1
+nssm set kubelet AppRotateOnline 1
+nssm set kubelet AppRotateSeconds 86400
+nssm set kubelet AppRotateBytes 1048576
