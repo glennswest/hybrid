c642295127247ee4cc0d2e70b102754278d186c5
“Glenn <glennswest@neuralcloudcomputing.com>
2018-06-25 14:00:27 +0000

    Update and cleanup
    	modified:   bin/start_kubeovn.ps1
    	modified:   windows.yml
    	modified:   windows_part2.yml

--- bin/start_kubeovn.ps1
+++ bin/start_kubeovn.ps1
@@ -1,1 +1,1 @@
-C:\bin\ovnkube.exe --init-node $env:computername  --config-file "C:\Program Files\Cloudbase Solutions\Open vSwitch\conf\ovn_k8s.conf" -cluster-subnet 10.128.0.0/14 -cni-conf-dir="C:\cni" -service-cluster-ip-range 172.30.0.0/16
+C:\bin\ovnkube.exe --init-node $env:computername  --config-file "C:\Program Files\Cloudbase Solutions\Open vSwitch\conf\ovn_k8s.conf" -cluster-subnet 10.128.0.0/14 -cni-conf-dir="C:\k\hybrid\cni" -service-cluster-ip-range 172.30.0.0/16
