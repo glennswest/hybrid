6eb8d45cda2f4afd7d538706ea70c206637cb22f
glennswest <glennswest@neuralcloudcomputing.com>
2018-06-28 08:39:41 -0500

    Add single-execution to docker-create.ps1 and change to use .lock extension

--- bin/setup-network.ps1
+++ bin/setup-network.ps1
@@ -1,7 +1,7 @@
-$a = Test-Path "C:\k\network_setup"
+$a = Test-Path "C:\k\network_setup.lock"
 IF ($a -eq "True") {Write-Host "Network Already Installed";exit}
 Write-Host "Installing Network"
-date > c:\k\network_setup
+date > c:\k\network_setup.lock
 
 $INTERFACE_ALIAS="Ethernet 3"
 Stop-Service ovs-vswitchd -force; Get-VMSwitch -SwitchType External | Disable-VMSwitchExtension "Cloudbase Open vSwitch Extension"
@@ -19,4 +19,3 @@ Set-NetAdapter -Name br-ex -MacAddress $MAC_ADDRESS -Confirm:$false
 # br-ex will get all the interface details from the DHCP server now
 Enable-NetAdapter br-ex
 Write-Host "SDN Network is setup"
-
