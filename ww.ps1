64794d8b1895d5f6370a048c2ddf4bfbe8e5a31c
“Glenn <glennswest@neuralcloudcomputing.com>
2018-07-29 12:07:37 -0500

    Test1

--- ww.ps1
+++ ww.ps1
@@ -1,2 +1,3 @@
-Get-NetRoute | Where-Object -FilterScript {$_.NextHop -Ne "::"} | Where-Object -FilterScript { $_.NextHop -Ne "0.0.0.0" } | Where-Object -FilterScript { ($_.NextHop.SubString(0,6) -Ne "fe80::") } | Get-NetAdapter
-
+$roughname = Get-NetRoute | Where-Object -FilterScript {$_.NextHop -Ne "::"} | Where-Object -FilterScript { $_.NextHop -Ne "0.0.0.0" } | Where-Object -FilterScript { ($_.NextHop.SubString(0,6) -Ne "fe80::") } | Get-NetAdapter
+$INTERFACE_ALIAS= $roughname.Substring(11)
+echo $INTERFACE_ALIAS
