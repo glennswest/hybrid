73c4d3acf710a550e39bc07fcc91df118608aa9c
gwest <gwest@redhat.com>
2018-07-31 07:12:48 -0700

    Fix crlf in interface name

--- bin/setup-network.ps1
+++ bin/setup-network.ps1
@@ -5,7 +5,7 @@ date > c:\k\network_setup.lock
 $ErrorActionPreference = "SilentlyContinue"
 
 $roughname = Get-NetRoute | Where-Object -FilterScript {$_.NextHop -Ne "::"} | Where-Object -FilterScript { $_.NextHop -Ne "0.0.0.0" } | Where-Object -FilterScript { ($_.NextHop.SubString(0,6) -Ne "fe80::") } | Get-NetAdapter | Format-List -Property "Name" | Out-String
-$INTERFACE_ALIAS= $roughname.Substring(11)
+$INTERFACE_ALIAS= $roughname.Substring(11) -replace "`n|`r",""
 echo $INTERFACE_ALIAS
 
 Stop-Service ovs-vswitchd -force; Get-VMSwitch -SwitchType External | Disable-VMSwitchExtension "Cloudbase Open vSwitch Extension"
